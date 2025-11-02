e0c014cfa80f9a2b8be0071265de60e0d4b3e9e0
Danfreitasp <danfreitasp@gmail.com>
2025-11-02 20:12:50 -0300

    Alteracoes de login

--- login.c
+++ login.c
@@ -1,7 +1,9 @@
 # include <stdio.h>
 int main(){
 
-    printf("Digite seu login");
-    printf("Login aceito");
+    printf("Digite seu login\n");
+    printf("Login aceito\n");
+    printf("Login aceito\n");
+    printf("Login aceito\n");
     return 0;
 }
